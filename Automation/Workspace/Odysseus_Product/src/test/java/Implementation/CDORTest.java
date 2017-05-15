a3338c3b25421f1da1e0c9fa46117fbc7cf4f3de
Ajitn123 <nakumajit@yahoo.com>
2017-05-15 16:24:15 +0530

    Update CDORTest.java
    Changed browser..

--- Automation/Workspace/Odysseus_Product/src/test/java/Implementation/CDORTest.java
+++ Automation/Workspace/Odysseus_Product/src/test/java/Implementation/CDORTest.java
@@ -66,17 +66,17 @@ public class CDORTest
        {
              if (browser.equalsIgnoreCase("Firefox"))
              {
-                //  driver = new FirefoxDriver();
+                   driver = new FirefoxDriver();
 	   
              } 
              else if (browser.equalsIgnoreCase("chrome")) 
              {
-            	        System.setProperty("webdriver.chrome.driver", "D:\\Ajit\\Driver\\chromedriver_win32\\chromedriver.exe");
-	                    DesiredCapabilities capabilities = DesiredCapabilities.chrome();
-	                    LoggingPreferences loggingprefs = new LoggingPreferences();
-	                    loggingprefs.enable(LogType.BROWSER, Level.ALL);
-	                    capabilities.setCapability(CapabilityType.LOGGING_PREFS, loggingprefs);
-	                    driver = new ChromeDriver(capabilities);
+            	     //       System.setProperty("webdriver.chrome.driver", "D:\\Ajit\\Driver\\chromedriver_win32\\chromedriver.exe");
+	             //       DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+	             //       LoggingPreferences loggingprefs = new LoggingPreferences();
+	             //       loggingprefs.enable(LogType.BROWSER, Level.ALL);
+	             //       capabilities.setCapability(CapabilityType.LOGGING_PREFS, loggingprefs);
+	             //       driver = new ChromeDriver(capabilities);
 	                   
              }  
              else if (browser.equalsIgnoreCase("IE")) 
