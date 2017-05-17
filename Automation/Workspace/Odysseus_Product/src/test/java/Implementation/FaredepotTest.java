92abc2cdb623cc5307d6111c93ab6260f3b67b7c
Ajitn123 <nakumajit@yahoo.com>
2017-05-17 16:02:41 +0530

    Update FaredepotTest.java

--- Automation/Workspace/Odysseus_Product/src/test/java/Implementation/FaredepotTest.java
+++ Automation/Workspace/Odysseus_Product/src/test/java/Implementation/FaredepotTest.java
@@ -18,6 +18,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.logging.LogEntries;
 import org.openqa.selenium.logging.LogEntry;
 import org.openqa.selenium.logging.LogType;
