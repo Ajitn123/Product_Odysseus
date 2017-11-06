c811b84e7b8a8a07c4c13a33de5f795bfebcae85
Ajitn123 <nakumajit@yahoo.com>
2017-11-06 20:45:44 +0530

    Update FaregeekTest.java

--- Automation/Workspace/Odysseus_Product/src/test/java/Implementation/FaregeekTest.java
+++ Automation/Workspace/Odysseus_Product/src/test/java/Implementation/FaregeekTest.java
@@ -492,7 +492,7 @@ public class FaregeekTest
 	           Thread.sleep(4000);
 	           
 	          
-	           
+/*	           
 	           
 	        // For Apply Best Price Guarantee   
 	        if(driver.findElement(By.cssSelector("#MainForm > div.col-md-9.col-xs-12.col-sm-9.pull-right.pax-main-cont > div.row.booking-section.satisfaction.panel-box.pax-info-mo > div > div > div > label [id*='ex_gr_']")).isDisplayed())
@@ -605,7 +605,7 @@ public class FaregeekTest
                  }
 	        	
 	        }
-	    
+*/	    
 	      try
 	      {
 	           //Click on Continue to Payment Button
